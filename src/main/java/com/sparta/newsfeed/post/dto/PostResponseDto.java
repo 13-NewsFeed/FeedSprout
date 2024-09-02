81c99d4bf2173f9becb42cc2e4f75512613916c3
이지택 <mkojibhu@naver.com>
2024-09-02 15:43:54 +0900

    post CRUD 기본 구현

---
+++ src/main/java/com/sparta/newsfeed/post/dto/PostResponseDto.java
@@ -0,0 +1,31 @@
+package com.sparta.newsfeed.post.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class PostResponseDto {
+
+    private Long id;
+    private Long userId;
+    private String title;
+    private String contents;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public PostResponseDto(Long id, Long userId, String title, String content) {
+        this.id = id;
+        this.userId = userId;
+        this.title = title;
+        this.contents = content;
+    }
+
+}
+
---
+++ src/main/java/com/sparta/newsfeed/post/service/PostService.java
@@ -0,0 +1,82 @@
+package com.sparta.newsfeed.post.service;
+
+
+import com.sparta.newsfeed.post.dto.PostRequestDto;
+import com.sparta.newsfeed.post.dto.PostResponseDto;
+import com.sparta.newsfeed.post.entity.Post;
+import com.sparta.newsfeed.post.entity.User;
+import com.sparta.newsfeed.post.repository.PostRepository;
+import com.sparta.newsfeed.post.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    public PostResponseDto create(Long userId, PostRequestDto dto) {
+        User user = userRepository.findById(userId).orElseThrow(() ->
+                new IllegalArgumentException("대상 유저가 없습니다."));
+        Post post = Post.createPost(dto, user);
+        Post savedPost = postRepository.save(post);
+
+        return new PostResponseDto(
+                savedPost.getId(),
+                savedPost.getUser().getId(),
+                savedPost.getTitle(),
+                savedPost.getContents(),
+                savedPost.getCreatedAt(),
+                savedPost.getModifiedAt()
+        );
+
+    }
+
+    public PostResponseDto getTodo(Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new NullPointerException("대상 게시글이 없습니다."));
+        PostResponseDto responseDto = new PostResponseDto(
+                post.getId(),
+                post.getUser().getId(),
+                post.getTitle(),
+                post.getContents(),
+                post.getCreatedAt(),
+                post.getModifiedAt()
+        );
+        return responseDto;
+    }
+
+    public PostResponseDto update(Long postId, PostRequestDto dto) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new NullPointerException("대상 게시글이 없습니다."));
+        post.update(
+                dto.getTitle(),
+                dto.getContents()
+        );
+        postRepository.save(post);
+        return new PostResponseDto(
+                post.getId(),
+                post.getUser().getId(),
+                post.getTitle(),
+                post.getContents(),
+                post.getCreatedAt(),
+                post.getModifiedAt()
+        );
+
+    }
+
+    public PostResponseDto delete(Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new NullPointerException("대상 게시글이 없습니다."));
+        postRepository.delete(post);
+        return new PostResponseDto(
+                post.getId(),
+                post.getUser().getId(),
+                post.getTitle(),
+                post.getContents(),
+                post.getCreatedAt(),
+                post.getModifiedAt()
+        );
+
+    }
+}
