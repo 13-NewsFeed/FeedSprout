ea64a9ceee5957e4e312f4f8696474ef3d5324ef
Jay3029 <lttleleepp@gmail.com>
2024-09-02 21:12:40 +0900

    인증 인가 완성

---
+++ src/main/java/com/sparta/newsfeed/auth/dto/LoginRequestDto.java
@@ -0,0 +1,14 @@
+package com.sparta.newsfeed.auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDto {
+
+    private String username;
+    private String email;
+    private String password;
+
+}
