3312f8c1058d945f3d6af48149b2f4be08d36dce
20240729 <yoohyeonsik@gmail.com>
2024-09-02 20:04:46 +0900

    update : 댓글 수정에도 시간 추가

--- src/main/java/com/sparta/newsfeed/comment/dto/CommentUpdateResponseDto.java
+++ src/main/java/com/sparta/newsfeed/comment/dto/CommentUpdateResponseDto.java
@@ -2,11 +2,23 @@ package com.sparta.newsfeed.comment.dto;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class CommentUpdateResponseDto {
+    private final Long id;
     private final String contents;
+    private final Long postId;
+    private final Long userId;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
 
-    public CommentUpdateResponseDto(String contents) {
+    public CommentUpdateResponseDto(Long id, String contents, Long postId, Long userId, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.id = id;
         this.contents = contents;
+        this.postId = postId;
+        this.userId = userId;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
     }
 }
