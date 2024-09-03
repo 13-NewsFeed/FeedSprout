285d4ce33c8bfecd8644c86a100083f2e7a00dc2
KTJ-1650 <xowns1650@gmail.com>
2024-09-03 14:49:52 +0900

    댓글,게시글 좋아요 완성

---
+++ src/main/java/com/sparta/newsfeed/like/entity/Like.java
@@ -0,0 +1,41 @@
+package com.sparta.newsfeed.like.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "likey")
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    public Like(User user, Post post){
+        setUser(user);
+        setPost(post);
+    }
+
+    public Like(User user, Comment comment){
+        setUser(user);
+        setComment(comment);
+    }
+
+}
---
+++ src/main/java/com/sparta/newsfeed/like/repository/LikeRepository.java
@@ -0,0 +1,11 @@
+package com.sparta.newsfeed.like.repository;
+
+import com.sparta.newsfeed.like.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    Optional<Like> findByUserAndPost(User user, Post post);
+    Optional<Like> findByUserAndComment(User user, Comment comment);
+}
