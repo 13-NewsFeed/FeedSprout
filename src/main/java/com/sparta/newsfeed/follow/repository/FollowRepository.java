499b7ce4c60a0bc34cbfb453a724bb8668e946c5
이지택 <mkojibhu@naver.com>
2024-09-06 13:30:32 +0900

    팔로우 게시글 조회 수정

--- src/main/java/com/sparta/newsfeed/follow/repository/FollowRepository.java
+++ src/main/java/com/sparta/newsfeed/follow/repository/FollowRepository.java
@@ -1,7 +1,10 @@
 package com.sparta.newsfeed.follow.repository;
 
 import com.sparta.newsfeed.follow.entity.Follow;
+import com.sparta.newsfeed.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +15,9 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
     boolean existsByFollowerIdAndFolloweeId(Long followerId, Long followeeId);
     List<Long> findFolloweeIdsByFollowerId(Long followerId);
     List<Long> findFollowerIdsByFolloweeId(Long followeeId);
+
+    @Query("SELECT f.followee FROM Follow f WHERE f.follower.id = :followerId")
+    List<User> findFollowedUsersByFollowerId(@Param("followerId") Long followerId);
 
   /*  // 나와 팔로우인 애들의 게시물 가져오기
     @Query("SELECT post FROM Post post WHERE post.user IN (" +
